995cb1198e6da3cac9674d386a89ab76a7a8211a
neeraj negi <nnegi637673@gmail.com>
2024-05-21 21:40:04 +0530

    changes made

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,14 @@
-
 import { initializeApp } from "firebase/app";
-import {  getFirestore } from "firebase/firestore";
-import { getAuth , GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 const firebaseConfig = {
-  apiKey :`${process.env.REACT_APP_API_KEY}` ,
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
   authDomain: `${process.env.REACT_APP_AUTH__DOMAIN}`,
-  projectId: `${process.env.  REACT_APP_PROJECT_ID}`, 
-  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET }`,
-  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID }` ,
-  appId: `${process.env.REACT_APP_API_ID }`
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_API_ID}`,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
